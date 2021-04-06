d4a376b114a8457b2b3940b4e5bbad985fc97fa2
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 00:56:18 +0200

    chore: move `meta` to `Layout` from `_document`

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -8,9 +8,10 @@ export interface LayoutProps {
 
 export function Layout({ children, title }: LayoutProps) {
     return (
-        <section className='p-4'>
+        <section className='container mx-auto py-4'>
             <Head>
                 <title>{title} - Mansa</title>
+                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             </Head>
             {children}
         </section>
