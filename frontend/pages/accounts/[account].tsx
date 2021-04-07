40ed27e437255075e0d0862750eb5870d0ce718d
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 20:07:02 +0200

    feat: add basic layout in `Account` component

--- frontend/pages/accounts/[account].tsx
+++ frontend/pages/accounts/[account].tsx
@@ -1,3 +1,9 @@
+import { Layout } from '~components/Layout';
+
 export default function Account() {
-    return <div></div>;
+    return (
+        <Layout title='View account'>
+            <div></div>
+        </Layout>
+    );
 }
