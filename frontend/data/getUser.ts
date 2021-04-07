2bea7312a067aab6b8255f73e006cceff2533a17
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 17:04:19 +0200

    feat: fetch more data for user and update type and data fetcher

--- frontend/data/getUser.ts
+++ frontend/data/getUser.ts
@@ -1,12 +1,11 @@
 import { User } from '~interfaces/user.decl';
 
-export async function getUser(): Promise<User> {
-    return await fetch(
-        'https://randomuser.me/api/?seed=getmansa&nat=fr&noinfo&exc=login,location,registered,id,dob'
-    )
+export async function getUser(): Promise<User | null> {
+    return await fetch('https://randomuser.me/api/?seed=getmansa&nat=fr&noinfo&exc=login')
         .then((res) => res.json())
         .then((data) => data.results?.[0] || {})
         .catch((error) => {
             console.error(error);
+            return null;
         });
 }
--- frontend/interfaces/user.decl.ts
+++ frontend/interfaces/user.decl.ts
@@ -14,4 +14,29 @@ export interface User {
         medium: string;
         thumbnail: string;
     };
+    dob: {
+        date: string;
+    };
+    location: {
+        street: {
+            number: number;
+            name: string;
+        };
+        city: string;
+        state: string;
+        country: string;
+        postcode: string | number;
+        coordinates: {
+            latitude: string;
+            longitude: string;
+        };
+        timezone: {
+            offset: string;
+            description: string;
+        };
+    };
+    registered: {
+        date: string;
+        age: number;
+    };
 }
