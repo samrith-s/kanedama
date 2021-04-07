ab25871e20c5adc16b2ff54b14f6b9cd74ec58f3
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 20:06:13 +0200

    refactor: add row gap and remove unwanted TW classes in `Grid`

--- frontend/components/Grid.tsx
+++ frontend/components/Grid.tsx
@@ -1,14 +1,15 @@
 import { rem } from 'polished';
-import tw, { styled } from 'twin.macro';
+import { styled } from 'twin.macro';
 
 export interface GridProps {
     columns?: string;
 }
 
 export const Grid = styled.div<GridProps>`
-    ${tw`grid auto-rows-max`};
+    display: grid;
     grid-template-columns: ${({ columns }) => columns};
     grid-auto-rows: max-content;
     column-gap: ${rem(20)};
+    row-gap: ${rem(20)};
     margin-bottom: ${rem(20)};
 `;
