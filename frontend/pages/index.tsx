8e1e5138dd284f54ee241fb7b45ba95ae5afa665
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 17:04:57 +0200

    feat: implement minimal UI with user data in index route

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,6 @@
+import { format } from 'date-fns';
+
+import { Grid } from '~components/Grid';
 import { Layout } from '~components/Layout';
 
 import { getUser } from '~data/getUser';
@@ -9,7 +12,26 @@ interface HomeProps {
 }
 
 export default function Home({ user }: HomeProps) {
-    return <Layout title='Profile'>{JSON.stringify(user, null, 2)}</Layout>;
+    return (
+        <Layout title='Profile'>
+            <Grid columns='max-content auto'>
+                <img
+                    src={user.picture.large}
+                    className='border border-gray-200 rounded'
+                    width='75px'
+                />
+                <div>
+                    <h2 className='text-lg font-bold'>
+                        {user.name.title}. {user.name.first} {user.name.last}
+                    </h2>
+                    <p>{user.email}</p>
+                    <p className='text-gray-500 text-sm'>
+                        Member since {format(new Date(user.registered.date), 'dd/MM/yyyy')}
+                    </p>
+                </div>
+            </Grid>
+        </Layout>
+    );
 }
 
 export async function getStaticProps() {
