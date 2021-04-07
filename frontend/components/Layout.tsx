4173730d4b4d9a8c20bbe8b7877bd71750515aad
Samrith Shankar <samrith.298@gmail.com>
2021-04-07 20:05:05 +0200

    feat: add progress bar on page change

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
 
 export interface LayoutProps {
     children: React.ReactNode;
@@ -12,6 +13,11 @@ export function Layout({ children, title }: LayoutProps) {
                 <title>{title} - Mansa</title>
                 <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             </Head>
+            <NextNProgress
+                color='#6347d1'
+                height={10}
+                options={{ easing: 'ease', speed: 500, showSpinner: false }}
+            />
             {children}
         </section>
     );
