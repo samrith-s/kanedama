a5605ed26b9c3e213c7f07be611480566ab88bb6
Samrith Shankar <samrith.298@gmail.com>
2021-04-10 23:18:48 +0200

    feat: create `Input` component

---
+++ frontend/components/Input.tsx
@@ -0,0 +1,6 @@
+import tw, { styled } from 'twin.macro';
+
+export const Input = styled.input`
+    outline: 0;
+    ${tw`px-4 py-2 transition border border-gray-200 rounded focus:border-primary`}
+`;
