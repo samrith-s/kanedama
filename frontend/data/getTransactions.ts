6df5e1c6dd233dadaddc2516091a4378765aeff8
Samrith Shankar <samrith.298@gmail.com>
2021-04-14 01:20:20 +0200

    refactor: update `getTransactions` to automatically add `from` and `to`

--- frontend/data/getTransactions.ts
+++ frontend/data/getTransactions.ts
@@ -1,34 +1,34 @@
-import { Transaction } from '~interfaces/transaction.decl';
-import { subDays } from 'date-fns';
+import { format } from 'date-fns';
 
-export async function getTransactions(
-    accountId: string,
-    options: {
-        from: string;
-        to: string;
-    } = {
-        from: subDays(new Date(), 365).toISOString(),
-        to: new Date().toISOString(),
-    }
-): Promise<Transaction[]> {
+import { Transaction } from '~interfaces/transaction.decl';
+
+export async function getTransactions(accountId: string, year?: number): Promise<Transaction[]> {
     if (!accountId) {
         throw new Error('An `accountId` should be provided to fetch transactions.');
     }
 
-    const { from, to } = options;
+    const formatString = "yyyy-MM-dd'T'HH:mm:ss";
+    const currentYear = new Date().getFullYear();
+    const from = format(new Date(`${year || currentYear}-01-01T00:00:00`), formatString);
+    let to = '';
 
-    let promise: Promise<any>;
-
-    if (options?.from && options?.to) {
-        promise = fetch(
-            `https://kata.getmansa.com/accounts/${accountId}/transactions?from=${from}&to=${to}`
-        );
+    if (!year || year === currentYear) {
+        to = new Date().toISOString();
     } else {
-        promise = fetch(`https://kata.getmansa.com/accounts/${accountId}/transactions`);
+        to = format(new Date(`${year}-12-31T23:59:59`), formatString);
     }
 
-    return await promise
+    return await fetch(
+        `https://kata.getmansa.com/accounts/${accountId}/transactions?from=${from}&to=${to}`
+    )
         .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                throw data;
+            }
+
+            return data;
+        })
         .catch((error) => {
             console.error(error);
             return [];
