1b5e82d3f94e8c5febdf98e98d0fa21453bc3821
Samrith Shankar <samrith.298@gmail.com>
2021-04-13 22:21:33 +0200

    feat: implement account card with information and distinctions

---
+++ frontend/components/AccountCard.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import { useMemo } from 'react';
+import tw, { styled } from 'twin.macro';
+import CurrencyList from 'currency-list';
+
+import { Card } from '~components/Card';
+
+import { Account } from '~interfaces/accounts.decl';
+
+interface AccountCardProps {
+    account: Account;
+}
+
+const AccountBadge = styled.div<{ accountType: Account['account_type'] }>`
+    ${tw`absolute top-0 right-0 text-sm py-1 px-2 rounded-bl`}
+    ${(props) =>
+        props.accountType === 'TRANSACTION'
+            ? tw`bg-yellow-500 text-yellow-900`
+            : tw`bg-green-500 text-green-900`}
+`;
+
+export function AccountCard({ account }: AccountCardProps) {
+    const { account_id, account_number, account_type, iban, current, currency } = account;
+
+    const cardClasses = useMemo(() => {
+        let classNames = 'relative overflow-hidden transition transform hover:scale-105 hover:z-10';
+        if (account.account_type === 'TRANSACTION') {
+            return classNames + ' border-yellow-500';
+        }
+
+        return classNames + ' border-green-500';
+    }, [account.account_type]);
+
+    const currencyInfo = useMemo(() => {
+        return CurrencyList.get(currency);
+    }, [currency]);
+
+    return (
+        <Link
+            href={`/accounts/${account_id}?data=${Buffer.from(JSON.stringify(account)).toString(
+                'base64'
+            )}`}
+        >
+            <a>
+                <Card className={cardClasses}>
+                    <AccountBadge accountType={account_type}>{account_type}</AccountBadge>
+                    <p className='text-xl'>
+                        <strong>{account_number}</strong>
+                    </p>
+                    <p className='text-sm text-gray-600'>{iban}</p>
+                    <div className='h-0.5 bg-gray-100 my-2' />
+                    <p className='text-right'>
+                        Balance: {currencyInfo.symbol}
+                        {current}
+                    </p>
+                </Card>
+            </a>
+        </Link>
+    );
+}
--- frontend/components/Card/index.tsx
+++ frontend/components/Card/index.tsx
@@ -2,13 +2,14 @@ import { ReactNode } from 'react';
 
 interface CardProps {
     children?: ReactNode;
-    title: string;
+    title?: string;
+    className?: string;
 }
 
-export function Card({ title, children }: CardProps) {
+export function Card({ title, children, className = '' }: CardProps) {
     return (
-        <div className='shadow-md rounded p-6 border border-gray-200'>
-            <h2 className='font-bold text-md mb-3.5 text-primary'>{title}</h2>
+        <div className={`shadow-md bg-white rounded p-6 border border-gray-200 ${className}`}>
+            {title && <h2 className='font-bold text-md mb-3.5 text-primary'>{title}</h2>}
             <div className='grid auto-rows-max grid-cols-1 sm:gap-1 gap-3'>{children}</div>
         </div>
     );
