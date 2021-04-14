cd3409ae56ee86b7b07c07699957c80ce9167a6b
Samrith Shankar <samrith.298@gmail.com>
2021-04-15 00:30:46 +0200

    test: add e2e test for home page

--- frontend/cypress/integration/home.spec.ts
+++ frontend/cypress/integration/home.spec.ts
@@ -7,6 +7,21 @@ describe('route: /', () => {
             .and('contain.text', 'INSEE 1NNaN46702872 66');
         cy.get('[data-testid=user-since')
             .should('exist')
-            .and('context.text', 'Member since 29/09/2017');
+            .and('contain.text', 'Member since 29/09/2017');
+    });
+
+    it('renders business info block', () => {
+        cy.visit('/');
+        cy.get('[data-testid=business-info]').should('exist');
+    });
+
+    it('renders personal info block', () => {
+        cy.visit('/');
+        cy.get('[data-testid=personal-info]').should('exist');
+    });
+
+    it('renders contact info block', () => {
+        cy.visit('/');
+        cy.get('[data-testid=contact-info]').should('exist');
     });
 });
