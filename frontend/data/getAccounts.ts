79d3131759484b75c406d7a27bf046491993d1a7
Samrith Shankar <samrith.298@gmail.com>
2021-04-14 03:17:43 +0200

    test: add test for `getAccounts`

--- frontend/data/getAccounts.ts
+++ frontend/data/getAccounts.ts
@@ -2,7 +2,7 @@ import { Account } from '~interfaces/accounts.decl';
 
 export async function getAccounts(): Promise<Account[]> {
     return await fetch('https://kata.getmansa.com/accounts')
-        .then((res) => res.json() || [])
+        .then((res) => res.json())
         .catch((error) => {
             console.error(error);
             return [];
